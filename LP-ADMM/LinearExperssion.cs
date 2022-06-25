7147507d6f7e80ef665091af89050276c210700e
rogierhans <rogierhans@gmail.com>
2022-06-25 13:49:07 +0200

    warning disable

--- LP-ADMM/LinearExperssion.cs
+++ LP-ADMM/LinearExperssion.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace LP_ADMM;
 
+#pragma warning disable CS0661 // Type defines operator == or operator != but does not override Object.GetHashCode()
+#pragma warning disable CS0660 // Type defines operator == or operator != but does not override Object.Equals(object o)
 public partial class LinearExperssion
+#pragma warning restore CS0660 // Type defines operator == or operator != but does not override Object.Equals(object o)
+#pragma warning restore CS0661 // Type defines operator == or operator != but does not override Object.GetHashCode()
 {
     public List<(double, Variable)> Vars = new();
     public double Constant;
